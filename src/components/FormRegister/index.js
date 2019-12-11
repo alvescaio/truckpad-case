df91487dfd931119912b582e62207b806a521909
Caio Alves <caio_alves@outlook.com>
2019-12-11 17:08:28 -0300

    Testes no dataPicker

--- src/components/FormRegister/index.js
+++ src/components/FormRegister/index.js
@@ -2,14 +2,17 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 import { Formik, Form, Field } from "formik";
 import DatePickerField from "./DatePickerField";
+import INITIAL_VALUES from "../Register/formInitialValues";
+
+INITIAL_VALUES.birth_date = Date();
 
 const FormRegister = () => {
   return (
-    <Formik onSubmit={(event) => alert(event.date)} initialValues={{ date: new Date() }}>
+    <Formik onSubmit={(e) => alert(e.birth_date)} initialValues={INITIAL_VALUES}>
       {({ values, errors }) => (
         <Form>
           <Grid container>
-            <Field name="date" component={DatePickerField} />
+            <Field name="birth_date" component={DatePickerField} />
           </Grid>
         </Form>
       )}
