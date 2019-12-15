784f09d0a0672d1e0131aae0e94506470c0c382f
Caio Alves <cacaioalves@gmail.com>
2019-12-15 20:27:51 -0300

    Updates

---
+++ precache-manifest.8edb0f47596872368d702cee9a861aba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae6564f6602f6625730a9cc458a8d64b",
+    "url": "/truckpad-case/index.html"
+  },
+  {
+    "revision": "f3d2577c373671c2add2",
+    "url": "/truckpad-case/static/css/main.6ae0c75a.chunk.css"
+  },
+  {
+    "revision": "252ef8f96f510d60f3e4",
+    "url": "/truckpad-case/static/js/2.f4df43a5.chunk.js"
+  },
+  {
+    "revision": "c8d937b44801a61947e20cab2e635a1c",
+    "url": "/truckpad-case/static/js/2.f4df43a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3d2577c373671c2add2",
+    "url": "/truckpad-case/static/js/main.700ca210.chunk.js"
+  },
+  {
+    "revision": "126127fddfe628c6a24a",
+    "url": "/truckpad-case/static/js/runtime-main.2e952ead.js"
+  },
+  {
+    "revision": "bd326b2f19f8092f6bbcba612a9b7398",
+    "url": "/truckpad-case/static/media/clear.bd326b2f.gif"
+  },
+  {
+    "revision": "50f2bca38b3d7c0c7c47c4a441d8c689",
+    "url": "/truckpad-case/static/media/trucker.50f2bca3.png"
+  },
+  {
+    "revision": "2b8369266035fa0eb6b3f02218c59503",
+    "url": "/truckpad-case/static/media/truckpad-logo.2b836926.svg"
+  }
+]);
